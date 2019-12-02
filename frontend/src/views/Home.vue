3b1b3f8940369bfa00331249987901600b1a54dd
Nayanchandra Patel <38898074+DoubleE420@users.noreply.github.com>
2019-12-01 22:30:45 -0500

    fix up homepage

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,78 +1,144 @@
 <template>
   <div>
-    <div class="relative">
-      <v-card
-        color="rgba(0, 0, 0, 0)"
-        class="ma-auto pa-4"
-        max-width="400"
+    <v-img
+      src="../../public/img/background_angle.png"
+      width="100%"
+      max-width="100%"
+      max-height="100%"
+      elevation="0"
+    >
+      <v-row
+        justify="space-around"
       >
-        <h1>
-          Easy analyst tools built by IT professionals.
-        </h1>
-        <p>
-          DataGrate is the most effortless solution for simplifying complex data migrations.
-        </p>
+        <v-card
+          class="pa-4 custom-mt"
+          width="400"
+        >
+          <h1 class="tw-text-4xl">
+            Easy analyst tools built by IT professionals.
+          </h1>
+          <p>
+            DataGrate is the most effortless solution for simplifying complex data migrations.
+          </p>
+          <v-row
+            justify="space-around"
+          >
+            <v-btn
+              color="primary_button"
+              class="mr-4"
+              width="40%"
+            >
+              Get Started
+            </v-btn>
+            <v-btn
+              color="secondary_button"
+              width="40%"
+            >
+              Learn More
+            </v-btn>
+          </v-row>
+        </v-card>
+        <v-card
+        ></v-card>
+      </v-row>
+    </v-img>
+    <v-container
+      class="tw--mt-64"
+    >
+      <v-sheet
+        class="mx-auto py-4"
+        color="red"
+        elevation="24"
+      >
+        <v-row
+          justify="space-around"
+          class=""
+        >
+          <v-card
+            max-width="30%"
+            class="pa-4"
+            flat
+          >
+            <v-img
+              src="../../public/img/data.png"
+              width="50px"
+              class="mx-auto"
+            >
+            </v-img>
+                      <!--<img style="width:50px; height: auto;" src="datagrate/data.png" alt="icon"/>-->
+            <v-card-title
+              class="justify-center"
+            >
+              Migration
+            </v-card-title>
+            <v-card-text>
+              Using our solution data can move from database to database with our simple pipelining framework.
+            </v-card-text>
+          </v-card>
+
+          <v-card
+            max-width="30%"
+            class="pa-4"
+            flat
+          >
+            <v-img
+              src="../../public/img/data.png"
+              width="50px"
+              class="mx-auto"
+            >
+            </v-img>
+            <v-card-title
+              class="justify-center"
+            >
+              Integration
+            </v-card-title>
+            <v-card-text>
+              By combining datasets, DataGrate allows to transform segmented data into a single cohesive unit that allows for simple management and analytics.
+            </v-card-text>
+          </v-card>
+
+          <v-card
+            max-width="30%"
+            class="pa-4"
+            flat
+          >
+            <v-img
+              src="../../public/img/data.png"
+              width="50px"
+              class="mx-auto"
+            >
+            </v-img>
+            <v-card-title
+              class="justify-center"
+            >
+              Management
+            </v-card-title>
+            <v-card-text>
+              The in-built suite of analytics tools slows for pulling and organizing data for all of your business needs.
+            </v-card-text>
+          </v-card>
+        </v-row>
         <v-row
           justify="space-around"
         >
           <v-btn
-            color="blue"
-            class="mr-4"
-            width="40%"
+            class="ma-4"
+            color="primary_button"
           >
-            Download
-          </v-btn>
-          <v-btn
-            color="light-blue"
-            width="40%"
-          >
-            Learn More
+            View Use Cases
           </v-btn>
         </v-row>
-      </v-card>
-    </div>
-    <v-content>
-      <v-container>
-        <v-row justify="space-around">
-          <v-col>
-            <p style="text-align: center;">
-            <!--<img style="width:50px; height: auto;" src="datagrate/data.png" alt="icon"/>-->
-            </p>
-            <p style="padding-top: 20px;">
-            <b>Migration</b>
-            <br>
-            Using our solution data can move from database to database with our simple pipelining framework.
-            </p>
-          </v-col>
-          <v-col>
-            <p style="text-align: center;">
-            <!--<img style="width:50px; height: auto;" src="datagrate/data.png" alt="icon"/>-->
-            </p>
-            <p style="padding-top: 20px;">
-            <b>Integration</b>
-            <br>
-            By combining datasets, DataGrate allows to transform segmented data into a single cohesive unit that allows for simple management and analytics.
-            </p>
-            <br><br><br>
-            <button class="button1">View Use Cases</button>
-          </v-col>
-          <v-col>
-            <p style="text-align: center;">
-            <!--<img style="width:50px; height: auto;" src="datagrate/data.png" alt="icon"/>-->
-            </p>
-            <p style="padding-top: 20px;">
-            <b>Management</b>
-            <br>
-            The in-built suite of analytics tools slows for pulling and organizing data for all of your business needs.
-            </p>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
+      </v-sheet>
+    </v-container>
   </div>
 </template>
 
 <style scoped lang="scss">
+.custom-mt {
+  margin-top: 15%;
+  margin-right: 5%;
+}
+
 ul {
   list-style-type: none;
   margin: 0;
@@ -100,26 +166,6 @@ li a:hover {
   background-color: #052C95;
 }
 
-.greeting {
-  padding-left: 300px;
-  line-height: 28px;
-  padding-top: 200px;
-  position: absolute;
-  width: 330px;
-  color: white;
-}
-
-.greeting h1{
-  text-align: left;
-  font-size:22px;
-  letter-spacing: 1px;
-}
-
-.greeting p{
-  font-size:16px;
-  line-height: 24px;
-}
-
 .button1 {
   background-color: #4DA6FF;
   border: none;
@@ -142,30 +188,6 @@ li a:hover {
   display: inline-block;
   font-size: 16px;
   border-radius: 10px;
-}
-
-#column {
-  float: left;
-  width: 220px;
-  margin: 45px;
-  color: #555555;
-  line-height: 18px;
-  letter-spacing: 0.5px;
-  text-align: left;
-}
-
-#column:after {
-    clear: both;
-    content: "";
-    display: block;
-}
-
-.main-container {
- min-height: 100vh; /* will cover the 100% of viewport */
- overflow: hidden;
- display: block;
- position: relative;
- padding-bottom: 100px; /* height of your footer */
 }
 
 footer{

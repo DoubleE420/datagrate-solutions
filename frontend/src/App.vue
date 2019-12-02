98a3f53f7fb911f47814a3767e191c3fdb237c9d
Nayanchandra Patel <38898074+DoubleE420@users.noreply.github.com>
2019-12-01 23:07:04 -0500

    change navbar to component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      hide-on-scroll
-      flat
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="DataGrate Logo"
-          class="shrink mr-2"
-          contain
-          src="../public/img/logo_trans.png"
-          transition="scale-transition"
-          width="100"
-        />
-      </div>
-      <v-spacer></v-spacer>
-      <v-btn
-        to="/"
-        class="mr-4"
-        color="secondary_button"
-      >
-        Home
-      </v-btn>
-      <v-btn
-        to="/dbreader"
-        color="secondary_button"
-      >
-        Database
-      </v-btn>
-    </v-app-bar>
-
+    <Nav/>
     <v-content>
       <v-overlay
         :value="loadingStatus"
@@ -59,10 +29,12 @@
 </style>
 
 <script>
+import Nav from '@/components/Nav.vue'
 import { mapState } from 'vuex'
 export default {
   name: 'App',
   components: {
+    Nav
   },
   data: () => ({
     //
---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar
+    app
+    hide-on-scroll
+    flat
+  >
+    <div class="d-flex align-center">
+      <v-img
+        alt="DataGrate Logo"
+        class="shrink mr-2"
+        contain
+        src="../../public/img/logo_trans.png"
+        transition="scale-transition"
+        width="100"
+      />
+    </div>
+    <v-spacer></v-spacer>
+    <v-btn
+      to="/"
+      class="mr-4"
+      color="secondary_button"
+    >
+      Home
+    </v-btn>
+    <v-btn
+      to="/dbreader"
+      color="secondary_button"
+    >
+      Database
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>

f07e1e025547f675a80c562ad941686ac8e6d651
Nayan Patel <38898074+DoubleE420@users.noreply.github.com>
2020-02-21 18:52:57 -0500

    update dbreader page

--- frontend/src/views/DBReader.vue
+++ frontend/src/views/DBReader.vue
@@ -1,158 +1,115 @@
 <template>
   <v-container>
     <v-content>
-      <v-btn
-        @click="getData(sourceDBName)"
+      <v-card
+        class="text-center pb-6"
       >
-        ALL DATA
-      </v-btn>
-
-      <v-btn
-        @click="getuseridData(); useridSQL() "
-      >
-        user_id data
-      </v-btn>
-
-      <v-btn
-        @click="getgenderData(); genderSQL() "
-      >
-        gender data
-      </v-btn>
-
-      <v-btn
-        @click="getnameData(); namesSQL() "
-      >
-        name data
-      </v-btn>
-        <v-btn
-        @click="getmaleData()"
-      >
-        Only Males
-      </v-btn>
-      <v-menu
-        offset-y
-      >
-        <template v-slot:activator="{ on }">
-          <v-btn
-            outlined
-            v-on="on"
+        <div class="tw-flex tw-flex-wrap justify-center align-center">
+          <v-card-title class="justify-center">Select the Table:</v-card-title>
+          <v-menu
+            offset-y
           >
-            <span>{{ firstStatement.name }}</span>
-            <v-icon right>mdi-menu-down</v-icon>
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item
-            v-for="(item, index) in firstStatementTypes"
-            :key="index"
-            @click="selFirstStatement(item)"
+            <template v-slot:activator="{ on }">
+              <v-btn
+                outlined
+                v-on="on"
+              >
+                <span>{{ currentTable }}</span>
+                <v-icon right>mdi-menu-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(item, index) in srcDBTables"
+                :key="index"
+                @click="selTable(item)"
+              >
+                <v-list-item-title>{{ getTableName(item) }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+        <div
+          v-show="selectedTable"
+        >
+          <hr class="mx-8 mt-4 tw-border-1 tw-border-gray-400"/>
+          <v-card-title
+            class="justify-center"
           >
-            <v-list-item-title>{{ item.name }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-      <v-menu
-        offset-y
-      >
-        <template v-slot:activator="{ on }">
-          <v-btn
-            outlined
-            v-on="on"
+            Select the column:
+          </v-card-title>
+          <div
+            class="tw-flex tw-flex-wrap justify-center"
           >
-            <span>{{ secondStatement }}</span>
-            <v-icon right>mdi-menu-down</v-icon>
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item
-            v-for="(item, index) in secondStatementColumns"
-            :key="index"
-            @click="selColumn(item)"
-          >
-            <v-list-item-title>{{ item.COLUMN_NAME }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-      <v-btn
-        outlined
-        disabled
-      >
-        {{ getThirdStatement }}
-      </v-btn>
-      <v-menu
-        offset-y
-      >
-        <template v-slot:activator="{ on }">
-          <v-btn
-            outlined
-            v-on="on"
-          >
-            <span>{{ fourthStatement }}</span>
-            <v-icon right>mdi-menu-down</v-icon>
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item
-            v-for="(item, index) in fourthStatementTables"
-            :key="index"
-            @click="selTable(item)"
-          >
-            <v-list-item-title>{{ getTableName(item) }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-      <v-btn
-        color="success"
-        @click="executeQuery()"
-      >Execute</v-btn>
-      <v-row>
-        <v-col>
-          <div v-for="dataItem in data" v-bind:key="dataItem.id" class="mt-4">
-            <v-card>
-              <v-card-title>
-                Username: {{ dataItem.username }}
-                <br>
-                ID: {{ dataItem.user_id }}
-              </v-card-title>
-              <v-card-subtitle>
-                {{ alldataSQL }}
-              </v-card-subtitle>
-              <v-card-text>
-                First Name: {{ dataItem.first_name }}
-                <br>
-                Last Name: {{ dataItem.last_name }}
-                <br>
-                Gender: {{ dataItem.gender }}
-                <br>
-                Password: {{ dataItem.password }}
-              </v-card-text>
-            </v-card>
+            <v-btn
+              class="ma-2"
+              @click="getData({
+                table: currentTable,
+                col: '*'
+              }); clickedData()"
+            >*</v-btn>
+            <div
+              v-for="(item, index) in srcTableColumns"
+              :key="index"
+              @click="selColumn(item)"
+              class="text-center"
+            >
+              <v-btn
+                class="ma-2"
+                @click="getData({
+                  table: currentTable,
+                  col: item.COLUMN_NAME
+                }); clickedData()"
+              >
+                {{ item.COLUMN_NAME }} Data
+              </v-btn>
+            </div>
           </div>
-        </v-col>
-        <v-col>
-          <div v-for="dataItem in data" v-bind:key="dataItem.id" class="mt-4">
-            <v-card>
-              <v-card-title>
-                Username: {{ dataItem.username }}
-                <br>
-                ID: {{ dataItem.user_id }}
-              </v-card-title>
-              <v-card-subtitle>
-                {{ alldataSQL }}
-              </v-card-subtitle>
-              <v-card-text>
-                First Name: {{ dataItem.first_name }}
-                <br>
-                Last Name: {{ dataItem.last_name }}
-                <br>
-                Gender: {{ dataItem.gender }}
-                <br>
-                Password: {{ dataItem.password }}
-              </v-card-text>
+        </div>
+      </v-card>
+      <v-card
+        v-if="selected"
+      >
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+          class="ma-4"
+        ></v-text-field>
+        <v-data-table
+          :items="data"
+          :headers="getHeaders(data)"
+          :items-per-page="5"
+          :search="search"
+          class="mt-4"
+        ></v-data-table>
+      </v-card>
+      <!--
+      <v-row>
+        <v-col
+          class="tw-flex tw-flex-wrap"
+        >
+          <div v-for="(dataItem, index) in data" v-bind:key="index" class="mt-4 tw-w-1/3">
+            <v-card
+              class="mx-4"
+            >
+              <div class="px-4 pt-4 pb-1">
+                <p v-for="(item, index) in dataItem" :key="index">
+                  <span class="tw-font-bold">
+                    {{ index }}:
+                  </span>
+                  <span>
+                    {{ item }}
+                  </span>
+                </p>
+              </div>
             </v-card>
           </div>
         </v-col>
       </v-row>
+      -->
     </v-content>
   </v-container>
 </template>
@@ -160,8 +117,6 @@
 <script>
 // @ is an alias to /src
 import { mapState, mapActions } from 'vuex'
-import JQuery from 'jquery'
-let $ = JQuery
 export default {
   name: 'DBReader',
   components: {
@@ -169,84 +124,66 @@ export default {
   data: () => ({
     firstStatement: 'None',
     secondStatement: '',
-    fourthStatement: 'None',
-    tableName: 'None'
+    currentTable: 'None',
+    tableName: 'None',
+    selectedTable: false,
+    selected: false,
+    search: ''
   }),
   mounted () {
-    this.readColumns()
-    this.readTables()
+    this.readSrcTables()
   },
   methods: {
     ...mapActions([
-      'readColumns',
-      'readTables'
+      'readSrcColumns',
+      'readSrcTables',
+      'getData'
     ]),
-    getData () {
-      this.$store.dispatch('getData')
-      console.log(this.data)
-    },
-    getuseridData () {
-      this.$store.dispatch('getuseridData')
-      console.log(this.data)
-    },
-    getgenderData () {
-      this.$store.dispatch('getgenderData')
-      console.log(this.data)
-    },
-    getnameData () {
-      this.$store.dispatch('getnameData')
-      console.log(this.data)
-    },
-    getmaleData () {
-      this.$store.dispatch('getmaleData')
-      console.log(this.data)
-    },
     selFirstStatement (item) {
       this.firstStatement = item
     },
     selColumn (col) {
-      console.log('selected column: ' + col.COLUMN_NAME)
       this.secondStatement = col.COLUMN_NAME
     },
     selTable (table) {
-      var tableName = Object.values(table)[0]
-      this.fourthStatement = tableName
+      var name = Object.values(table)[0]
+      this.currentTable = name
+      this.selectedTable = true
     },
     getTableName (item) {
-      var name = Object.values(item)[0]
-      var val = Object.keys(item)[0]
-      console.log(name, val)
-      this.tableName = name
-      var data = `{
-        ` + val + ': ' + name + ` 
-      }`
-      var obj = JSON.parse(data)
-      var result = $.parseJSON(obj)
-      $.each(result, function (k, v) {
-        alert(k + ' is ' + v)
-      })
+      return Object.values(item)[0]
     },
-    executeQuery () {
-      var query = 'yay'
-      console.log(query)
+    clickedData () {
+      this.selected = true
+    },
+    getHeaders (data) {
+      var head = []
+      if (data) {
+        var keys = Object.keys(data[0])
+        for (var i = 0; i < keys.length; ++i) {
+          head.push(
+            {
+              'text': keys[i],
+              'value': keys[i]
+            }
+          )
+        }
+      }
+      console.log(head)
+      return head
     }
   },
   computed: {
-    alldataSQL () {
-      return 'test sql statement return'
-    },
     ...mapState([
       'data',
       'currentDB',
-      'firstStatementTypes',
-      'secondStatementColumns',
-      'fourthStatementTables'
-    ]),
-    getThirdStatement () {
-      if (this.firstStatement.id === 1 && this.secondStatement) {
-        return 'FROM'
-      }
-      return 'NONE'
+      'srcTableColumns',
+      'srcDBTables'
+    ])
+  },
+  watch: {
+    currentTable: function (val) {
+      this.readSrcColumns(val)
     }
   }
 }
